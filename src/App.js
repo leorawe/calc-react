f1cfdb51d2d75e2f5073cac38ff1cd4c2fc2df6e
Leora Wenger <web@leoraw.com>
2019-01-03 17:14:30 -0500

    more calculation

--- src/App.js
+++ src/App.js
@@ -29,20 +29,73 @@ const Top = styled.div`
     `;   
 //change to class and add state
 class App extends React.Component{
-   state = {recent: 0};
+   state = {
+       recent: 0,
+       calculation: null,
+       operator: 0
+    };
+
    // set a callback here - onResultChange
    onResultChange = (newVal) => {
-     //  console.log(someArg);
-     this.setState(() => ({recent: newVal}));
+     this.setState(() => ({recent: newVal}));  
+     this.doCalculation(newVal);
+     
+     //console.log(this.state.recent);
      //also, do some calculation then add this to state
    };
 
+   doCalculation = (arg) =>{
+    switch(arg){
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+            //if recent is a number or a decimal
+            console.log(this.state.recent);
+            let recent = this.state.recent;
+            let regex = /^\d+$/;
+            regex.test(recent)? console.log('num'): console.log('other');
+            this.setState(() => ({calculation: arg}));
+            break;  
+      case 'C':
+          this.setState(() => ({calculation: 0}));
+          break;
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+         //console.log(arg);
+          //let newSum = parseFloat(this.state.recent) + parseFloat(this.state.calculation);
+          this.setState(() => ({operator: arg}));
+          console.log(this.state.operator);
+          break;
+      case '=':
+            console.log('op', arg, this.state.calculation);
+            //do operations here
+           // let newSum = parseFloat(this.state.recent) + parseFloat(this.state.calculation);
+               
+          break;
+      default:
+          this.setState(() => ({calculation: arg}));
+   }
+ }
+
    onScreen =  () => {
        //this should be the calculated result instead of recent
-       let val = this.state.recent;
+       let val = (this.state.calculation!=null)? this.state.calculation: this.state.recent;
       switch(val){
          case 'C':
             return '0';
+         case '+':
+            return val;  
+         case '=':
+            return val;    
          default:
             return val;   
       }
